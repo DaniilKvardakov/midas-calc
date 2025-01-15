008644697452b507b031d3885f9f1ec654d050bc
adept <adeptiumion@gmail.com>
2025-01-15 17:13:47 +0700

    feat: Упрощение Swagger конфига, используя аннотационный подход.

--- backend/midas/src/main/java/midas/configurations/SwaggerConfiguration.java
+++ backend/midas/src/main/java/midas/configurations/SwaggerConfiguration.java
@@ -1,55 +1,20 @@
 package midas.configurations;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-import java.util.List;
-
-@Configuration
-@PropertySource("classpath:secret/swagger.properties")
-public class SwaggerConfiguration  {
-
-    @Value("${swagger.server}")
-    private String serverUrl;
-    @Value("${swagger.title}")
-    private String title;
-    @Value("${swagger.api.version}")
-    private String apiVersion;
-    @Value("${swagger.contact.url}")
-    private String contactUrl;
-    @Value("${swagger.contact.name}")
-    private String contactName;
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        return new OpenAPI()
-                .servers(
-                        List.of(
-                                new Server()
-                                        .url(serverUrl)
-                        )
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Midas API",
+                version = "1.0",
+                description = "Midas API маленького \"подпивасного\" сервиса.",
+                contact = @Contact(
+                        name = "Telegram",
+                        url = "https://t.me/Trust_me_enjoyer"
                 )
-                .info(
-                        new Info()
-                                .title(title)
-                                .version(apiVersion)
-                                .description("Midas API маленького \"подпивасного\" сервиса.")
-                                .contact(
-                                        new Contact()
-                                                .name(contactName)
-                                                .url(contactUrl)
-                                )
-
-                );
-    }
-
-
-
-}
+        )
+)
+@Configuration
+public class SwaggerConfiguration  { }
